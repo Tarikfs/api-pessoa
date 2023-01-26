54d94f7ad43d80065cd0d330a274297087343f83
Tarik Santos <tsantos@vertigo.com.br>
2023-01-25 23:13:43 -0300

    "add dependencia e altera classe teste padrão"

--- src/test/java/tarikfs/apipessoa/ApiPessoaApplicationTests.java
+++ src/test/java/tarikfs/apipessoa/ApiPessoaApplicationTests.java
@@ -1,9 +1,7 @@
 package tarikfs.apipessoa;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ApiPessoaApplicationTests {
 
 	@Test
