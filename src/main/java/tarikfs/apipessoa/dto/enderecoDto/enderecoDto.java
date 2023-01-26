003c6b8f7637a6645458ba0e595d3d34e6a13679
Tarik Santos <tsantos@vertigo.com.br>
2023-01-25 22:19:17 -0300

    "gera Class enderecoDto"

---
+++ src/main/java/tarikfs/apipessoa/dto/enderecoDto/enderecoDto.java
@@ -0,0 +1,51 @@
+package tarikfs.apipessoa.dto.enderecoDto;
+
+public class enderecoDto {
+
+    private String logradouro;
+    private String cep;
+    private String numero;
+    private String cidade;
+
+    public enderecoDto() {
+    }
+
+    public enderecoDto(String logradouro, String cep, String numero, String cidade) {
+        this.logradouro = logradouro;
+        this.cep = cep;
+        this.numero = numero;
+        this.cidade = cidade;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+}
