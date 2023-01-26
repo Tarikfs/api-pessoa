5d4b996ce413aff11692e8ff4fab00a5d52d0df6
Tarik Santos <tsantos@vertigo.com.br>
2023-01-26 20:40:16 -0300

    "cria o metodo de criar pessoa"

--- src/main/java/tarikfs/apipessoa/controller/PessoaController.java
+++ src/main/java/tarikfs/apipessoa/controller/PessoaController.java
@@ -1,10 +1,28 @@
 package tarikfs.apipessoa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import tarikfs.apipessoa.dto.pessoaDto.PessoaDto;
+import tarikfs.apipessoa.service.PessoaService;
+
+@RequestMapping("/pessoa")
 @RestController
 public class PessoaController {
 
-    
+    @Autowired
+    PessoaService pessoaService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<PessoaDto> createPessoa(@RequestBody PessoaDto pessoaDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(pessoaService.createPessoa(pessoaDto));
+    }
 
 }
--- src/main/java/tarikfs/apipessoa/service/PessoaService.java
+++ src/main/java/tarikfs/apipessoa/service/PessoaService.java
@@ -3,7 +3,10 @@ package tarikfs.apipessoa.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
+import tarikfs.apipessoa.dto.pessoaDto.PessoaDto;
 import tarikfs.apipessoa.mapper.PessoaMapper;
+import tarikfs.apipessoa.model.Pessoa;
 import tarikfs.apipessoa.repository.PessoaRepository;
 
 @Service
@@ -15,4 +18,10 @@ public class PessoaService {
     @Autowired
     PessoaRepository pessoaRepository;
 
+    @Transactional
+    public PessoaDto createPessoa(PessoaDto pessoaDto) {
+        Pessoa pessoa = pessoaMapper.toModelPessoa(pessoaDto);
+        pessoaRepository.save(pessoa);
+        return pessoaMapper.toDtoPessoa(pessoa);
+    }
 }
