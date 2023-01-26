a82890b446401b858200b454bb7643bd9e72feb4
Tarik Santos <tsantos@vertigo.com.br>
2023-01-25 21:21:42 -0300

    "criação de classes model"

--- src/main/java/tarikfs/apipessoa/model/Endereco.java
+++ src/main/java/tarikfs/apipessoa/model/Endereco.java
@@ -9,4 +9,47 @@ public class Endereco {
     private String cep;
     private String numero;
     private String cidade;
+
+    public Endereco() {
+    }
+
+    public Endereco(String logradouro, String cep, String numero, String cidade) {
+        this.logradouro = logradouro;
+        this.cep = cep;
+        this.numero = numero;
+        this.cidade = cidade;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
 }
--- src/main/java/tarikfs/apipessoa/model/Pessoa.java
+++ src/main/java/tarikfs/apipessoa/model/Pessoa.java
@@ -22,7 +22,7 @@ public class Pessoa implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Size (min = 3, max = 50)
+    @Size(min = 1, max = 50)
     private String nome;
 
     @JsonFormat(pattern = "dd/MM/yyyy")
@@ -30,5 +30,51 @@ public class Pessoa implements Serializable {
 
     @OneToMany
     Endereco endereco;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(Long id, @Size(min = 1, max = 50) String nome, Date dataNascimento, Endereco endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.DataNascimento = dataNascimento;
+        this.endereco = endereco;
+    }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNascimento() {
+        return DataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        DataNascimento = dataNascimento;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
 
 }
