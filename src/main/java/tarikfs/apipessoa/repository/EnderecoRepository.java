7ebc835b64cd4ebf0168a7abf6435abc750b12ce
Tarik Santos <tsantos@vertigo.com.br>
2023-01-25 23:30:22 -0300

    "criação repositorio para endereço"

---
+++ src/main/java/tarikfs/apipessoa/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package tarikfs.apipessoa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tarikfs.apipessoa.model.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+
+}
